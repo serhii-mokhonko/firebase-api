ba3ff56fd66246c15e3a4580b881b48dd0ae7372
Ivan Mokhonko <ivan.mokhonko@onereach.com>
2020-03-08 20:29:04 +0200

    firebase-admin dependency added

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,8 @@
 const functions = require("firebase-functions");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
+const admin = require('firebase-admin');
+admin.initializeApp();
+
 exports.helloWorld = functions.https.onRequest((request, response) => {
   //  response.send("Hello from Firebase!");
 //   console.log("Hello World OK OK OK OK");
