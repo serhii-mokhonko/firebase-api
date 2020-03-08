68e5268774f9a0f9d8fb0f1994a2db0a7ee05051
sergey mokhonko <sergiy35@gmail.com>
2020-03-08 21:00:39 +0200

    Added dependency loadash

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,8 @@ const functions = require("firebase-functions");
 const admin = require('firebase-admin');
 admin.initializeApp();
 
+var _ = require('lodash');
+
 exports.helloWorld = functions.https.onRequest((request, response) => {
   //  response.send("Hello from Firebase!");
 //   console.log("Hello World OK OK OK OK");
