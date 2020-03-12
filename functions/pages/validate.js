d60374252ae3352b4853e156533eb16d2eeb82a8
Ivan Mokhonko <imohonyko@gmail.com>
2020-03-12 21:34:56 +0200

    fix(validateFirebaseToken): function interface improved

--- functions/pages/validate.js
+++ functions/pages/validate.js
@@ -1,20 +1,16 @@
 const admin = require("firebase-admin");
-const validateFirebaseIdToken = async (req, res, next) => {
-  console.log("Check if request is authorized with Firebase ID token");
 
+exports.validateFirebaseIdToken = async (req) => {
   if (
     (!req.headers.authorization ||
       !req.headers.authorization.startsWith("Bearer ")) &&
     !(req.cookies && req.cookies.__session)
   ) {
-    console.error(
-      "No Firebase ID token was passed as a Bearer token in the Authorization header.",
-      "Make sure you authorize your request by providing the following HTTP header:",
-      "Authorization: Bearer <Firebase ID Token>",
-      'or by passing a "__session" cookie.'
-    );
-    res.status(403).send("Unauthorized");
-    return;
+    return {
+      authenticated: false,
+      status: 403,
+      message: `Unauthorized`
+    };
   }
 
   let idToken;
@@ -22,29 +18,29 @@ const validateFirebaseIdToken = async (req, res, next) => {
     req.headers.authorization &&
     req.headers.authorization.startsWith("Bearer ")
   ) {
-    console.log('Found "Authorization" header');
-    // Read the ID Token from the Authorization header.
     idToken = req.headers.authorization.split("Bearer ")[1];
   } else if (req.cookies) {
-    console.log('Found "__session" cookie');
-    // Read the ID Token from cookie.
     idToken = req.cookies.__session;
   } else {
-    // No cookie
-    res.status(403).send("Unauthorized");
-    return;
+    return {
+      authenticated: false,
+      status: 403,
+      message: `Unauthorized`
+    };
   }
   try {
     const decodedIdToken = await admin.auth().verifyIdToken(idToken);
-    console.log("ID Token correctly decoded", decodedIdToken);
     req.user = decodedIdToken;
-    next();
-    return;
+    return {
+      authenticated: true,
+    };
   } catch (error) {
-    console.error("Error while verifying Firebase ID token:", error);
-    res.status(403).send("Unauthorized");
+    return {
+      authenticated: false,
+      status: 403,
+      message: `Unauthorized`
+    };
     return;
   }
 };
 
-exports.validateFirebaseIdToken = validateFirebaseIdToken;
