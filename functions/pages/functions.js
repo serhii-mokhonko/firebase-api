a316247e264910748be28135688b32adbb10267e
Ivan Mokhonko <ivan.mokhonko@onereach.com>
2020-06-20 14:21:57 +0300

    feat(pages)

---
+++ functions/pages/functions.js
@@ -0,0 +1,182 @@
+const admin = require('firebase-admin');
+const _ = require('lodash');
+const { RESPONSE_MESSAGES } = require('../response-messages');
+
+const checkValue = async (key, ref) => {
+    const snapshot = await admin.database().ref(`/${ref}/${key}`).once('value');
+    return snapshot.val();
+}
+
+exports.getPages = async (startAt, count) => {
+    if(_.isEmpty(startAt) || _.isEmpty(count))
+        return{
+            success: false,
+            message: RESPONSE_MESSAGES.REJECT.NEWS.PARAMS_NOT_SET
+        };
+
+    try {
+        const dbRecords =  await admin.database().ref('/pages').once('value');
+        const keys = Object.keys(dbRecords.val()).reverse();
+        const key = keys[parseInt(startAt)]
+
+        if(parseInt(startAt) >= keys.length)
+            return {
+                success: false,
+                message: RESPONSE_MESSAGES.REJECT.NEWS.OUT_OF_RANGE
+            };
+    
+        const query = admin.database().ref('/pages').orderByKey().limitToLast(parseInt(count)).endAt(key);
+        const snapshot = await query.once('value');
+        const data = snapshot.val();
+        if(!data)
+            return {
+                success: false,
+                massege: RESPONSE_MESSAGES.REJECT.NEWS.NOT_FOUND
+            };
+
+        const transformedData = transformData(data);
+
+        return {
+            success: true,
+            data: transformedData.reverse(),
+            newsCount: keys.length
+        };
+
+    } catch (err) {
+        return {
+            success: false,
+            error: err.message,
+            message: RESPONSE_MESSAGES.REJECT.NEWS.GET_DATA
+        }
+    }
+};
+
+exports.searchPages = async (str, startAt, itemsOnPage) => {
+    const searchStr =  _.toLower(str);
+    let result;
+    try{
+        const query = await admin.database().ref('pages').once('value');
+        const data = query.val();
+        const transformedData = transformData(data);
+
+        result = transformedData.reverse().filter(el => {
+            const title =  _.toLower(el.title);
+            const content = _.toLower(el.content);
+
+            return ( title.includes(searchStr) || content.includes(searchStr) );
+        });
+
+    }catch(err) {
+        return {
+            success: false,
+            message: RESPONSE_MESSAGES.REJECT.SEARCH.ERROR,
+            error: err.message
+        };
+    }
+
+    if(result.length > 0) {
+        if(!_.isEmpty(startAt) || !_.isEmpty(itemsOnPage)) {
+            const start = parseInt(startAt);
+            const end = start + parseInt(itemsOnPage);
+            const cuttedOutResult = result.slice(start, end);
+            return {
+                success: true,
+                data: cuttedOutResult,
+                newsCount: result.length
+            };
+        }
+
+        return {
+            success: true,
+            data: result,
+            count: result.length
+        };
+    }
+
+    return {
+        success: false,
+        message: RESPONSE_MESSAGES.REJECT.SEARCH.NOT_FOUND
+    };
+};
+
+exports.getSingleRecord = async (key) => {
+    const record = await admin.database().ref('pages').child(key).once('value');
+    if(!record.val())
+        return {
+            success: false,
+            message: RESPONSE_MESSAGES.REJECT.NEWS.ITEM_NOT_FOUND
+        };
+
+    return {
+        success: true,
+        data: record.val()
+    };
+}
+
+exports.addPage = async (data) => {
+    if(!data.title || !data.content)
+        return {
+            success: false,
+            message: RESPONSE_MESSAGES.REJECT.NEWS.FIELDS_EMPTY
+        };
+    
+    data.description = data.description || "";
+    data.visible = !_.isBoolean(data.visible) ? true : data.visible;
+    data.created = Date.now();
+
+    const snapshot = await admin.database().ref('/pages').push(data);
+
+    return {
+        success: true,
+        message: RESPONSE_MESSAGES.SUCCESS.NEWS.CREATED,
+        key: snapshot.key
+    }
+};
+
+exports.editPage = async (key, newData) => {
+    const value = await checkValue(key, 'pages');
+    if(!value) 
+        return {
+            success: false,
+            message: RESPONSE_MESSAGES.REJECT.NEWS.ITEM_NOT_FOUND
+        };
+
+    if(!newData.title || !newData.content)
+        return {
+            success: false,
+            message: RESPONSE_MESSAGES.REJECT.NEWS.FIELDS_EMPTY
+        };
+
+    newData.description = newData.description || "";
+    newData.visible = !_.isBoolean(newData.visible) ? true : newData.visible;
+    newData.updated = Date.now();
+
+    await admin.database().ref(`/pages/${key}`).update(newData);
+        return {
+            success: true,
+            message: RESPONSE_MESSAGES.SUCCESS.NEWS.EDITED,
+        };
+};
+
+exports.deletePage = async (key) => {
+    const value = await checkValue(key, 'pages');
+    if(!value) 
+        return {
+            success: false,
+            message: RESPONSE_MESSAGES.REJECT.NEWS.ITEM_NOT_FOUND
+        };
+    
+    await admin.database().ref(`/pages/${key}`).remove();
+    return {
+        success: true,
+        message: RESPONSE_MESSAGES.SUCCESS.NEWS.DELETED,
+    };
+};
+
+const transformData = (obj) => {
+    const arr = [];
+    _.forIn(obj, (val, key) => {
+        arr.push(_.assign(val, {'id': key}));
+    })
+    return arr;
+};
--- functions/pages/index.js
+++ functions/pages/index.js
@@ -1,157 +1,61 @@
-const { pages, admin } = require('./setup.js')
-const { RESPONSE_MESSAGES } = require('../response-messages.js')
-const { authenticate } = require('./authenticate');
-const _ =  require('lodash')
+const _ = require('lodash');
+const express = require('express');
+const pages = express();
+const { getPages, getSingleRecord, addPage, editPage, deletePage, searchPages } = require('./functions');
 
-const checkValue = async (key, ref) => {
-    const snapshot = await admin.database().ref(`/${ref}/${key}`).once('value');
-    return snapshot.val();
-}
-
-const transformData = (data) => {
-    let arr = [];
-    _.forIn(data, (val, key) => {
-        arr.push(_.assign(val, {'id': key}));
-    })
-    return arr;
-}
-
-const getPages = async () => {
-    const snapshot = await admin.database().ref('/pages').once('value');
-    const data = snapshot.val();
-    if(!data)
-        return {
-            success: false,
-            massege: RESPONSE_MESSAGES.REJECT.PAGES.NOT_FOUND
-        }
-
-    const result = transformData(data);
-    
-    return {
-        success: true,
-        data: result
-    }
-}
-
-const getPage = async (key) => {
-    let value = await checkValue(key, 'pages');
-    if(!value) 
-        return {
-            success: false,
-            message: RESPONSE_MESSAGES.REJECT.PAGES.KEY_NOT_FOUND
-        }
-
-    const data = _.assign(value, {'id': key});
-
-    return {
-        success: true,
-        data
-    }
-
-}
-
-const addPage = async ({ title, body, userId, visible = false }) => {
-    if(_.isEmpty(title) || _.isEmpty(body)) 
-        return {
-            success: false,
-            message: RESPONSE_MESSAGES.REJECT.PAGES.FIELDS_EMPTY
-        }
-
-    const snapshot = await admin.database().ref('/pages').push({title, body, userId, visible})
-    return {
-        success: true,
-        message: RESPONSE_MESSAGES.SUCCESS.PAGES.CREATED,
-        key: snapshot.key
-    }
-}
-
-const editPage = async ({key, newData}) => {
-    const value = await checkValue(key, 'pages');
-    if(!value) 
-        return {
-            success: false,
-            message: RESPONSE_MESSAGES.REJECT.PAGES.KEY_NOT_FOUND
-        }
-
-    const { title, body } = newData;
-    if(_.isEmpty(title) || _.isEmpty(body)) 
-        return {
-            success: false,
-            message: RESPONSE_MESSAGES.REJECT.PAGES.FIELDS_EMPTY
-        }
-
-    await admin.database().ref(`/pages/${key}`).update(newData);
-        return {
-            success: true,
-            message: RESPONSE_MESSAGES.SUCCESS.PAGES.EDITED,
-        }
-}
-
-const deletePage = async (key) => {
-    const value = await checkValue(key, 'pages');
-    if(!value) 
-        return {
-            success: false,
-            message: RESPONSE_MESSAGES.REJECT.PAGES.KEY_NOT_FOUND
-        }
-
-    if(_.isEmpty(key)) 
-        return {
-            success: false,
-            message: RESPONSE_MESSAGES.REJECT.PAGES.KEY_REQUIRED
-        }
-    
-    await admin.database().ref(`/pages/${key}`).remove()
-    return {
-        success: true,
-        message: RESPONSE_MESSAGES.SUCCESS.PAGES.DELETED,
-    }
-}
-
-pages.get('/:key', async (req, res) => {
-    const pageKey = req.params.key;
-    let result = await getPage(pageKey);
-
-    const responseStatus = result.success ? 200 : 400;
-    res.status(responseStatus).json(result);
-})
+const { authenticate } = require('../authenticate');
 
 pages.get('/', async (req, res) => {
-    const result = await getPages();
-    
-    const responseStatus = result.success ? 200 : 400;
-    res.status(responseStatus).json(result);
-})
+  let { startAt, itemsOnPage, q } = req.query;
+  let result;
+  if(!_.isEmpty(q)){
+    result = await searchPages(q, startAt, itemsOnPage);
+  } else {
+    result = await getPages(startAt, itemsOnPage);
+  }
+
+  const status = result.success ? 200 : 400;
+  res.set('Access-Control-Allow-Origin', '*');
+  res.status(status).json(result);
+});
+
+pages.get("/:key", async (req, res) => {
+  const key = req.params.key;
+  const result = await getSingleRecord(key);
+  const status = result.success ? 200 : 400;
+  res.set('Access-Control-Allow-Origin', '*');
+  res.status(status).json(result);
+});
 
 pages.post('/', async (req, res) => {
-    const {title, body, visible} = req.body;
-    
-    const isLoggedIn = await authenticate(req);
-    const result = isLoggedIn.authenticated ? await addPage({title, body, userId: isLoggedIn.userID, visible}) : isLoggedIn;
-   
-    const responseStatus = result.success ? 201 : 400;
-    res.status(responseStatus).json(result);
-})
+  const isLoggedIn = await authenticate(req);
+  const { title, description, content, visible } = req.body;
+  const result = isLoggedIn.authenticated 
+    ? await addPage({ title, content, authorId: isLoggedIn.userID, description, visible }) 
+    : isLoggedIn;
+  res.set('Access-Control-Allow-Origin', '*');
+  res.json(result);
+});
 
-pages.put(`/:key`, async (req, res) => {
-    const pageKey = req.params.key;
-    const {title, body, visible} = req.body;
-
-    const isLoggedIn = await authenticate(req);
-    const result = isLoggedIn.authenticated ? await editPage({ key: pageKey, newData: {title, body, userId: isLoggedIn.userID, visible}}) : isLoggedIn;
-    
-    const responseStatus = result.success ? 200 : 400;
-    res.status(responseStatus).json(result);
-})
+pages.put('/:key', async (req, res) => {
+  const key = req.params.key;
+  const { title, description, content, visible } = req.body;
+  const isLoggedIn = await authenticate(req);
+  const result = isLoggedIn.authenticated 
+    ? await editPage(key, { title, description, content, userIdUpdate: isLoggedIn.userID, visible }) 
+    : isLoggedIn;
+  res.set('Access-Control-Allow-Origin', '*');
+  res.json(result);
+});
 
 pages.delete('/:key', async (req, res) => {
-    const pageKey = req.params.key;
+  const key = req.params.key;
 
-    const isLoggedIn = await authenticate(req);
-    const result = isLoggedIn.authenticated ? await deletePage(pageKey) : isLoggedIn;
-    
-    const responseStatus = result.success ? 200 : 400;
-    res.status(responseStatus).json(result);
-})
+  const isLoggedIn = await authenticate(req);
+  const result = isLoggedIn.authenticated ? await deletePage(key) : isLoggedIn;
+  res.set('Access-Control-Allow-Origin', '*');
+  res.json(result);
+});
+
 
 exports.pages = pages;
